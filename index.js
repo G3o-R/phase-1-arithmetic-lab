c287f769258c47d931f1fb8e3e0ba39cba518c38
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-09-20 15:00:49 -0700

    commit tree 1a3d60794b36f084444d5573524e98c9b3acf9bf

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2)
+
+const random = (Math.floor(Math.random()*10)+1)
+
+let num3=16
+let num4=6
+let mod = (num3 %= num4)
+
+const max = (Math.max(1,2,3,4,5,6,20))
